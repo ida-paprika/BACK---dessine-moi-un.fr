3fca79a4381e07dc847c6af51fa3287335b9edd4
Paprika <ida.oussada@gmail.com>
2022-06-06 14:23:00 +0200

    update data scripts

--- dessine-moi-un-db/ddl/schema.ddl.sql
+++ dessine-moi-un-db/ddl/schema.ddl.sql
@@ -4,8 +4,19 @@
  * \i schema.ddl.sql
  * \q
  */
+ALTER TABLE IF EXISTS artists DROP CONSTRAINT fk_artists_profile;
+ALTER TABLE IF EXISTS artworks DROP CONSTRAINT fk_artworks_artist;
+ALTER TABLE IF EXISTS projects DROP CONSTRAINT fk_projects_artist;
+ALTER TABLE IF EXISTS projects DROP CONSTRAINT fk_projects_profile;
+ALTER TABLE IF EXISTS projects DROP CONSTRAINT fk_projects_art_medium;
+ALTER TABLE IF EXISTS projects DROP CONSTRAINT fk_projects_art_format;
+ALTER TABLE IF EXISTS projects DROP CONSTRAINT fk_projects_progress_status;
+ALTER TABLE IF EXISTS artists_art_mediums DROP CONSTRAINT fk_artists_art_mediums_artist;
+ALTER TABLE IF EXISTS artists_art_mediums DROP CONSTRAINT fk_artists_art_mediums_medium;
+ALTER TABLE IF EXISTS artists_art_formats DROP CONSTRAINT fk_artists_art_formats_artist;
+ALTER TABLE IF EXISTS artists_art_formats DROP CONSTRAINT fk_artists_art_formats_format;
 
-
+DROP TABLE IF EXISTS profiles;
 CREATE TABLE profiles (
 	id SERIAL PRIMARY KEY,
     uuid UUID UNIQUE NOT NULL,
@@ -14,6 +25,7 @@ CREATE TABLE profiles (
     last_name VARCHAR(50)
 );
 
+DROP TABLE IF EXISTS artists;
 CREATE TABLE artists (  
 	id SERIAL PRIMARY KEY,
 	artist_name VARCHAR(50) UNIQUE NOT NULL,
@@ -22,6 +34,7 @@ CREATE TABLE artists (
     profile_id INTEGER
 );
 
+DROP TABLE IF EXISTS artworks;
 CREATE TABLE artworks (
     id SERIAL PRIMARY KEY,
     file_name VARCHAR(255) UNIQUE NOT NULL,
@@ -29,12 +42,14 @@ CREATE TABLE artworks (
     artist_id INTEGER
 );
 
+DROP TABLE IF EXISTS art_mediums;
 CREATE TABLE art_mediums (  
 	id SERIAL PRIMARY KEY,
 	label VARCHAR(50) NOT NULL,
 	minimum_price INTEGER NOT NULL
 );
 
+DROP TABLE IF EXISTS art_formats;
 CREATE TABLE art_formats (  
 	id SERIAL PRIMARY KEY,
 	label VARCHAR(20) NOT NULL,
@@ -42,23 +57,27 @@ CREATE TABLE art_formats (
     multiplier NUMERIC(2,1) NOT NULL
 );
 
+DROP TABLE IF EXISTS artists_art_mediums;
 CREATE TABLE artists_art_mediums (
   artist_id INTEGER,
   art_medium_id INTEGER,
   PRIMARY KEY (artist_id, art_medium_id)
 );
 
+DROP TABLE IF EXISTS artists_art_formats;
 CREATE TABLE artists_art_formats (
   artist_id INTEGER,
   art_format_id INTEGER,
   PRIMARY KEY (artist_id, art_format_id)
 );
 
+DROP TABLE IF EXISTS progress_status;
 CREATE TABLE progress_status (  
 	id SERIAL PRIMARY KEY,
 	status VARCHAR(12) NOT NULL
 );
 
+DROP TABLE IF EXISTS projects;
 CREATE TABLE projects (  
 	id SERIAL PRIMARY KEY,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -87,5 +106,3 @@ ALTER TABLE artists_art_mediums ADD CONSTRAINT fk_artists_art_mediums_medium FOR
 
 ALTER TABLE artists_art_formats ADD CONSTRAINT fk_artists_art_formats_artist FOREIGN KEY(artist_id) REFERENCES artists(id);
 ALTER TABLE artists_art_formats ADD CONSTRAINT fk_artists_art_formats_format FOREIGN KEY(art_format_id) REFERENCES art_formats(id);
-
-
